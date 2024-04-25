eabe3060d352c421c11b415f48312cd5fa3ea219
Maya Jerndahl <144351614+MjBonk@users.noreply.github.com>
2024-04-25 15:40:11 +0200

    popup text

--- src/components/VideoPopup.jsx
+++ src/components/VideoPopup.jsx
@@ -1,28 +1,35 @@
-import React from 'react';
-import VimeoPlayer from './VimeoPlayer';
-import CloseCursor from '../assets/close.png';
+import React from "react";
+import VimeoPlayer from "./VimeoPlayer";
+import CloseCursor from "../assets/close.png";
 
-const VideoPopup = ({ onClose, videoId }) => {
-
+const VideoPopup = ({ onClose, videoId, client, campaign, composer }) => {
   return (
     <div
-      className='fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-90 animate-fadeIn'
+      className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-90 animate-fadeIn"
       style={{ cursor: `url(${CloseCursor}), auto` }}
       onClick={onClose}
     >
-      <div className='relative'>
+      <div className="relative">
         {/* Close button */}
         <div
-          className='relative top-4 right-4 cursor-pointer z-50 text-white animate-fadeInVideoPlaceholder'
+          className="relative top-4 right-4 cursor-pointer z-50 text-white animate-fadeInVideoPlaceholder"
           onClick={onClose}
         >
           X
         </div>
 
         {/* VimeoPlayer component */}
-        <div className='w-full h-full flex items-center justify-center bg-accent animate-fadeInVideoPlaceholder aspect-video'>
-          <VimeoPlayer videoId={videoId} className="animate-fadeInVideoPlaceholder" />
+        <div className="w-full h-full flex items-center justify-center bg-accent animate-fadeInVideoPlaceholder aspect-video">
+          <VimeoPlayer
+            videoId={videoId}
+            className="animate-fadeInVideoPlaceholder"
+          />
         </div>
+        <p className=" font-thin  md:text-white">
+          <span className=" uppercase">{client}</span>
+          <span>{campaign ? ` — ${campaign}` : null}</span>
+        </p>
+        <span className=" md:text-white">{composer}</span>
       </div>
     </div>
   );
