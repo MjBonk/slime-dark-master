71fb856b723135842655ec36002fd8470e68acf4
Maya Jerndahl <144351614+MjBonk@users.noreply.github.com>
2024-04-25 15:47:12 +0200

    title

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -8,7 +8,8 @@ export default function ProjectPage() {
 
 	return (
 		<>
-			<div className="text-center py-20 p-6">
+			<div className="flex flex-col justify-center py-20 p-6">
+			<h1 className="text-3xl md:text-5xl font-thin uppercase text-white py-10">All projects by our composers</h1>
 				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 					{projects.map((project, index) => (
 						<VideoThumbnail project={project} showComposer={true} index={index} key={index} />
